996bc046452f9cce4519fd0d583ebb89ab88869b
KimMinGuk <alsrnr315@gmail.com>
2022-10-26 15:24:36 +0900

    Update testruby.rb

--- .github/workflows/testruby.rb
+++ .github/workflows/testruby.rb
@@ -4,4 +4,19 @@ JWT_ISSUER_ID = ARGV[0]
 JWT_KEY_ID = ARGV[1]
 JWT_PRIVATE_KEY_CODE = ARGV[2]
 
-puts JWT_PRIVATE_KEY_CODE
+private_key = OpenSSL::PKey.read(JWT_PRIVATE_KEY_CODE)
+
+token = JWT.encode(
+   {
+    iss: JWT_ISSUER_ID,
+    exp: Time.now.to_i + 20 * 60,
+    aud: "appstoreconnect-v1"
+   },
+   private_key,
+   "ES256",
+   header_fields={
+     kid: JWT_KEY_ID
+   }
+)
+
+puts token
