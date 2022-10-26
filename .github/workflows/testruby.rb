47561bd21370d5b2fd8c70a2d74ec8c83ffe4f3c
KimMinGuk <alsrnr315@gmail.com>
2022-10-27 02:11:56 +0900

    Update testruby.rb

--- .github/workflows/testruby.rb
+++ .github/workflows/testruby.rb
@@ -1,19 +1,18 @@
-require "jwt"
+require "json"
 
 TEST = ARGV[0]
 
 data = {
-  "data" : [ {
-    "attributes": {
-       "name": "Archive-For-Release" 
-     }
-  }, {
-     "attributes": {
-       "name": "Archive-For-TestFlight" 
-     }
-  }
-    
-  ]
-}
+	"data" => [{
+			"attributes" => {
+				"name" => "Archive-For-Release"
+			}
+		}, {
+			"attributes" => {
+				"name" => "Archive-For-TestFlight"
+			}
+		}
+	]
+}.to_json
 
 puts data["data"].find {|data| data["attributes"]["name"] == "Archive-For-TestFlight"}
