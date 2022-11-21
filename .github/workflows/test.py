7ee5ec928ef00103ed025eb5188aec11a8f1b8c8
KimMinGuk <alsrnr315@gmail.com>
2022-11-21 16:14:55 +0900

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -2,8 +2,9 @@ import os
 
 shuoldSkipUpdate = os.environ['should_skip']
 
-if shuoldSkipUpdate == True:
-    print("hello")
-    exit()
+print(shuoldSkipUpdate)
+# if shuoldSkipUpdate == True:
+#     print("hello")
+#     exit()
 
 print("good")
